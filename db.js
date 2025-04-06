79ea70e0929ffac37957d3bcb97bb3e03e633956
Niels <nielsvandenbrande123outlook.com>
2025-04-06 21:22:41 +0200

    Add MongoDB support with Mongoose

--- db.js
+++ db.js
@@ -1,19 +1,15 @@
-const mongoose = require('mongoose');
+echo 'const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    // Replace this with your MongoDB connection string
     const connectionString = process.env.MONGODB_URI;
     
-    await mongoose.connect(connectionString, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log('MongoDB connected!');
+    await mongoose.connect(connectionString);
+    console.log("MongoDB connected!");
   } catch (err) {
-    console.error('MongoDB connection error:', err);
+    console.error("MongoDB connection error:", err);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+module.exports = connectDB;' > db.js
